acc51816fa9e7c243d9df773d29db28c68783789
Tooffeer <castillobrennan64@gmail.com>
2025-05-17 14:37:01 -0700

    Making test.py functional

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
+from waveshare_epd import epd2in13_V2
 from PIL import Image, ImageDraw, ImageFont
+import time
+
+epd = epd2in13_V2.EPD()
+epd.init(epd.FULL_UPDATE)
+epd.Clear(0xFF) 
 
 img = Image.new('1', (250, 122), 255)
 draw = ImageDraw.Draw(img)
@@ -6,4 +12,6 @@ font = ImageFont.load_default()
 
 draw.text((10, 10), "Hello World!", font = font, fill = 0)
 
-img.show()
+epd.display(epd.getbuffer(img))
+time.sleep(2)
+epd.sleep()
